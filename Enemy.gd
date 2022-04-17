2df05453cfd5618f4c1c50160c9322b6ef6305b1
CRBroughton <crbroughton@posteo.uk>
2022-04-17 21:34:26 +0100

    on enemy death, emit died signal

--- Enemy.gd
+++ Enemy.gd
@@ -5,10 +5,13 @@ var hp = 25 setget set_hp
 onready var hpLabel = $HPLabel
 onready var animationPlayer = $AnimationPlayer
 
+signal died
+
 func set_hp(new_hp):
 	hp = new_hp
 	hpLabel.text = str(hp)+'hp'
 	
 	if hp <= 0:
+		emit_signal("died")
 		queue_free() # destroys current node
 	animationPlayer.play('Shake')
