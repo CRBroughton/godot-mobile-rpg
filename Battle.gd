25a08ee2558e60315d5c857c24044ff3ece0ef23
CRBroughton <crbroughton@posteo.uk>
2022-04-17 21:34:46 +0100

    enemy set to null on emit, hide sword button

--- Battle.gd
+++ Battle.gd
@@ -1,7 +1,13 @@
 extends Node
 
+onready var enemy = $Enemy
+onready var swordButton = $UI/SwordButton
+
 func _on_SwordButton_pressed():
-	var enemy = find_node('Enemy')
-	
 	if enemy != null: 
 		enemy.hp -= 4
+
+
+func _on_Enemy_died():
+	swordButton.hide()
+	enemy = null
