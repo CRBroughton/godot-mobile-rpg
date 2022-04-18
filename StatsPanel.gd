5935fa54ed0e5e1718d46ca0c8ca1df4ee53eff0
CRBroughton <crbroughton@posteo.uk>
2022-04-18 09:42:29 +0100

    connected emitted values to panel

--- StatsPanel.gd
+++ StatsPanel.gd
@@ -4,3 +4,15 @@ onready var hpLabel = $StatsContainer/HP
 onready var apLabel = $StatsContainer/AP
 onready var mpLabel = $StatsContainer/MP
 
+
+
+func _on_PlayerStats_hp_changed(value):
+	hpLabel.text = 'HP\n'+str(value)
+
+
+func _on_PlayerStats_ap_changed(value):
+	hpLabel.text = 'AP\n'+str(value)
+
+
+func _on_PlayerStats_mp_changed(value):
+	hpLabel.text = 'MP\n'+str(value)
