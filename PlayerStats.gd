4527f882832238fc311f8109f04bc23d3bb29ff5
CRBroughton <crbroughton@posteo.uk>
2022-04-18 09:26:59 +0100

    created functions for player, added types

--- PlayerStats.gd
+++ PlayerStats.gd
@@ -1,10 +1,19 @@
 extends Node
 
-var max_hp = 25
+var max_hp: int = 25
 var hp = max_hp setget set_hp
-var max_ap = 3
+var max_ap: int = 3
 var ap = max_ap setget set_ap
-var max_mp = 10
+var max_mp: int = 10
 var mp = max_mp setget set_mp
 
 signal end_turn
+
+func set_hp(value: int) -> void:
+	hp = value
+	
+func set_ap(value: int) -> void:
+	ap = value
+	
+func set_mp(value: int) -> void:
+	mp = value
