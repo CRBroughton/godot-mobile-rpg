34e32104b2eb234ff6dce87be28879c060e2ab11
CRBroughton <crbroughton@posteo.uk>
2022-04-18 10:12:54 +0100

    animation now plays on correct frame

--- Enemy.gd
+++ Enemy.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var hp = 25 setget set_hp
+var target = null
 
 onready var hpLabel = $HPLabel
 onready var animationPlayer = $AnimationPlayer
@@ -15,12 +16,14 @@ func set_hp(new_hp):
 func attack(target) -> void:
 	yield(get_tree().create_timer(0.4), 'timeout') # gets base root node for current scene, waits for a set period of time
 	animationPlayer.play("Attack")
+	self.target = target
 	yield(animationPlayer, 'animation_finished')
+	self.target = null
 	target.hp -= 3
 	emit_signal("end_turn")
 	
 func deal_damage():
-	print('deal dmg')
+	self.target.hp -= 4
 
 func take_damage(amount):
 	self.hp -= amount
