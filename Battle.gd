4acacd641603701d7c775b464c071196f92522ec
CRBroughton <crbroughton@posteo.uk>
2022-04-24 10:19:37 +0100

    enemies are now created on room enter, ap is reset on each room

--- Battle.gd
+++ Battle.gd
@@ -7,8 +7,10 @@ export(Array, PackedScene) var enemies = []
 onready var battleActionButtons = $UI/BattleActionButtons
 onready var animationPlayer = $AnimationPlayer
 onready var nextRoomButton = $UI/CenterContainer/NextRoomButton
+onready var enemyPosition = $EnemyPosition
 
 func _ready():
+	randomize()
 	start_player_turn()
 	var enemy = BattleUnits.Enemy
 	
@@ -22,17 +24,21 @@ func start_enemy_turn():
 		enemy.attack()
 		yield(enemy, 'end_turn')
 	start_player_turn()
-		
+
 func start_player_turn():
 	battleActionButtons.show()
 	var playerStats = BattleUnits.PlayerStats
 	playerStats.ap = playerStats.max_ap
 	yield(playerStats, 'end_turn')
 	start_enemy_turn()
-	
-func create_new_enemy():
-	print('create enemy')
 
+func create_new_enemy():
+	enemies.shuffle()
+	var Enemy = enemies.front()
+	var enemy = Enemy.instance()
+	enemyPosition.add_child(enemy)
+	enemy.connect('died', self, '_on_Enemy_died')
+	
 func _on_Enemy_died():
 	nextRoomButton.show()
 	battleActionButtons.hide()
@@ -41,5 +47,7 @@ func _on_NextRoomButton_pressed():
 	nextRoomButton.hide()
 	animationPlayer.play("FadeToNewRoom")
 	yield(animationPlayer, 'animation_finished')
+	var playerStats = BattleUnits.PlayerStats
+	playerStats.ap = playerStats.max_ap
 	battleActionButtons.show()
 	create_new_enemy()
