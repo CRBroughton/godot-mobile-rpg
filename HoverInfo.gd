bd6fec0acb1a96a9fde58cac9c642058f919f751
CRBroughton <crbroughton@posteo.uk>
2022-04-24 11:13:30 +0100

    hooked up hover values to textbox

--- HoverInfo.gd
+++ HoverInfo.gd
@@ -3,8 +3,15 @@ extends Control
 export (String, MULTILINE) var description = ''
 
 func _on_HoverInfo_mouse_entered():
-	print(description)
+	var main = get_tree().current_scene
+	var textbox = main.find_node('Textbox')
+	if textbox != null:
+		textbox.text = description
 
 
 func _on_HoverInfo_mouse_exited():
-	pass
+	var main = get_tree().current_scene
+	var textbox = main.find_node('Textbox')
+	if textbox != null:
+		textbox.text = ''
+
